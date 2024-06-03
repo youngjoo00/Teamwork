8b700891c3d9775cb7bc2f13c68256752a6a0e21
youngjoo00 <yungjoo0624@gmail.com>
2024-06-03 23:56:34 +0900

    first commit

---
+++ Teamwork/TeamworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamworkApp.swift
+//  Teamwork
+//
+//  Created by youngjoo on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
