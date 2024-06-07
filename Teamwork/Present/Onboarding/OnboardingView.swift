5b3cb4c74425b1e9a74d65f4bd6fb0e4ffd46835
youngjoo00 <yungjoo0624@gmail.com>
2024-06-07 23:32:08 +0900

    Add: TCA 추가

---
+++ Teamwork/Present/Onboarding/OnboardingView.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingView.swift
+//  Teamwork
+//
+//  Created by youngjoo on 6/4/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
